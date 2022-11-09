bb30442c88fb9a9b50b4c5637303714ffb4e2279
karza <karalov.new@yandex.ru>
2022-11-09 20:33:52 +0400

    My HW massive part 1 (remaster)

--- src/Massive1.java
+++ src/Massive1.java
@@ -1,34 +1,57 @@
+import java.util.Arrays;
+
 public class Massive1 {
     public static void main(String[] args) {
 
         //Massive HomeWork - 1
-        //Task 1
-        int[] numsIntMassive = new int[] { 1, 2, 3 };
-        double[] numsDoubleMassive = { 1.57, 7.654, 9.986 };
-        String[] strMassive = { "I love cats", "I love my life", "I love chips" };
+        System.out.println("Task 1");
+        int[] numsIntMassive = new int[]{1, 2, 3};
+        double[] numsDoubleMassive = {1.57, 7.654, 9.986};
+        String[] strMassive = {"I love cats", "I love my life", "I love chips"};
 
-        //Task 2
-        System.out.println(numsIntMassive [0] + ", " + numsIntMassive [1] + ", " + numsIntMassive [2]);
-        System.out.println(numsDoubleMassive [0] + ", " + numsDoubleMassive [1] + ", " + numsDoubleMassive [2]);
-        System.out.println(strMassive [0] + ", " + strMassive [1] + ", " + strMassive [2]);
+        System.out.println("Task 2");
+
+        for (int i : numsIntMassive) {
+            if (i == numsIntMassive.length) {
+                System.out.print(i);
+            } else {
+                System.out.print(i + ", ");
+            }
+        }
+
+        System.out.println("\n");
+
+        for (int i = 0; i < numsDoubleMassive.length; i++) {
+            if (i == numsDoubleMassive.length - 1) {
+                System.out.print(numsDoubleMassive[i] + " ");
+            } else {
+                System.out.print(numsDoubleMassive[i] + ", ");
+            }
+        }
 
         System.out.println("\n");
 
-        //Task 3
-        System.out.println(numsIntMassive [2] + ", " + numsIntMassive [1] + ", " + numsIntMassive [0]);
-        System.out.println(numsDoubleMassive [2] + ", " + numsDoubleMassive [1] + ", " + numsDoubleMassive [0]);
-        System.out.println(strMassive [2] + ", " + strMassive [1] + ", " + strMassive [0]);
+        for (int i = 0; i < strMassive.length; i++) {
+            if (i == strMassive.length - 1) {
+                System.out.print(strMassive[i] + " ");
+            } else {
+                System.out.print(strMassive[i] + ", ");
+            }
+        }
 
         System.out.println("\n");
+
+        System.out.println("Task 3");
+
 
         //Task 4
 
-        for (int i : numsIntMassive) {
-            if (i % 2 != 0) {
-                System.out.println(i + 1);
+        for (int i = 0; i < numsIntMassive.length; i++) {
+            if (numsIntMassive[i] % 2 != 0) {
+                numsIntMassive[i] += 1;
             }
-
         }
+        System.out.println(Arrays.toString(numsIntMassive));
 
     }
 }
